ed0f1f69e0231554c184d5060b42b87fb79dd186
herodotus <pointer_v@qq.com>
2021-09-14 21:50:04 +0800

    1. 修复单体版Knife4j依赖错误问题 2. 修复单体版配置错误问题 3. 增加Gitee流程模版 4. 修复OAuth2自定义confirm_access.html,error.html,login.html页面，数据类型编译错误。 5. 优化自定义页面显示内容，增加Exception StackTrace输出 6. 修复XssUtils校验出错问题。 7. 优化ResourceServer安全配置 8. 修复OAuth2 四种模式中授权码模式（Authorization Code）、隐式授权模式（Implicit Grant）模式出错问题。 9. 解决前端控制台出错问题 10. 更新在线文档，增加OAuth2 四种模式验证说明

--- src/components/layouts/DefaultLayoutDrawer.vue
+++ src/components/layouts/DefaultLayoutDrawer.vue
@@ -2,16 +2,33 @@
     <v-navigation-drawer v-model="showDrawer" clipped app dark>
         <v-list dense>
             <template v-for="item in drawerMenu">
-                <v-list-group v-if="item.items" :key="item.title" :group="item.group" :prepend-icon="item.icon" no-action>
+                <v-list-group
+                    v-if="item.items"
+                    :key="item.title"
+                    :group="item.group"
+                    :prepend-icon="item.icon"
+                    no-action
+                >
                     <template v-slot:activator>
                         <v-list-item-content>
-                            <v-list-item-title v-text="item.title"></v-list-item-title>
+                            <v-list-item-title
+                                v-text="item.title"
+                            ></v-list-item-title>
                         </v-list-item-content>
                     </template>
                     <template v-for="subItem in item.items">
-                        <v-list-item :key="subItem.name" :to="generateChildTarget(item, subItem)" :href="subItem.href" :disabled="subItem.disabled" :target="subItem.target" class="ml-2">
+                        <v-list-item
+                            :key="subItem.name"
+                            :to="generateChildTarget(item, subItem)"
+                            :href="subItem.href"
+                            :disabled="subItem.disabled"
+                            :target="subItem.target"
+                            class="ml-2"
+                        >
                             <v-list-item-content>
-                                <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                                <v-list-item-title
+                                    v-text="subItem.title"
+                                ></v-list-item-title>
                             </v-list-item-content>
                             <v-list-item-icon>
                                 <v-icon v-text="subItem.icon"></v-icon>
@@ -22,13 +39,25 @@
                 <v-subheader v-else-if="item.header" :key="item.name">{{
                     item.header
                 }}</v-subheader>
-                <v-divider v-else-if="item.divider" :key="item.name"></v-divider>
-                <v-list-item v-else :to="!item.href ? { name: item.name } : null" :href="item.href" :disabled="item.disabled" :target="item.target" :key="item.name">
+                <v-divider
+                    v-else-if="item.divider"
+                    :key="item.name"
+                ></v-divider>
+                <v-list-item
+                    v-else
+                    :to="!item.href ? { name: item.name } : null"
+                    :href="item.href"
+                    :disabled="item.disabled"
+                    :target="item.target"
+                    :key="item.name"
+                >
                     <v-list-item-icon v-if="item.icon">
                         <v-icon v-text="item.icon"></v-icon>
                     </v-list-item-icon>
                     <v-list-item-content>
-                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                        <v-list-item-title
+                            v-text="item.title"
+                        ></v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
             </template>
@@ -39,11 +68,25 @@
 <script>
 export default {
     props: {
-        showDrawer: Boolean,
+        value: {
+            type: Boolean,
+            default: true
+        },
         drawerMenu: {
             type: Array,
             default: () => []
         }
+    },
+
+    computed: {
+        showDrawer: {
+            get () {
+                return this.value
+            },
+            set (value) {
+                this.$emit('input', value)
+            },
+        },
     },
 
     data: () => ({}),
