386e9eb5cf6a6d6d6e0f0e6fb5011bbed2b7a2f5
码匠君 <pointer_v@qq.com>
2023-06-01 14:19:25 +0800

    feat: 调整组件目录以及引用错误问题

--- packages/ui/src/components/widgets/manage/index.ts
+++ packages/ui/src/components/widgets/manage/index.ts
@@ -6,7 +6,6 @@ import HDictionarySelect from './HDictionarySelect.vue';
 import HOrganizationSelect from './HOrganizationSelect.vue';
 import HChangePassword from './HChangePassword.vue';
 import HChooseServer from './HChooseServer.vue';
-import HHttpMethodAvatar from './HHttpMethodAvatar.vue';
 import HSocialSignInList from './HSocialSignInList.vue';
 import HSendMessageField from './HSendMessageField.vue';
 import HSendMessageToUser from './HSendMessageToUser.vue';
@@ -20,7 +19,6 @@ export {
   HOrganizationSelect,
   HChangePassword,
   HChooseServer,
-  HHttpMethodAvatar,
   HSocialSignInList,
   HSendMessageField,
   HSendMessageToUser
