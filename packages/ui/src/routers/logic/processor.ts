442a7c1a4a635f7e70ba4f4c2b585ec39317eb4f
码匠君 <pointer_v@qq.com>
2024-04-09 23:37:42 +0800

    feat: merge James7 pr

--- packages/ui/src/routers/logic/processor.ts
+++ packages/ui/src/routers/logic/processor.ts
@@ -10,7 +10,7 @@ const vueModules = import.meta.glob('../../views/**/*.vue');
 
 /**
  * 将后端返回的路由 JSON 转换为前端可识别的格式，主要解决 vite 环境下，component 的 import 问题
- * @param dataimport { ModuleNamespace } from 'vite/types/hot';
+ * @param data import { ModuleNamespace } from 'vite/types/hot';
 import default from './../../../vite.config';
 
  * @returns
@@ -109,9 +109,60 @@ export const addRoutes = (router: Router, routes: Array<RouteRecordRaw>) => {
   }
 };
 
+/**
+ * 在不修改数据库数据情况下，修复后端Workbench路由，避免左侧菜单不支持toCreate详情页问题
+ * 如果修改了数据库，可取消此方法
+ * 修改数据库数据：
+ * ```sql - postgresql
+ * update sys_element
+ *   set component = null,
+ *   hide_all_child = false
+ *   where path = '/dashboard/workbench';
+ * update sys_element
+ *   set component = 'views/pages/workflow/process-approve/Index.vue',
+ *   detail_content = false,
+ *   title = '审批流程'
+ *   where path = '/dashboard/workbench/process-approve';
+ * update sys_element
+ *   set component = 'views/pages/workflow/process-start/Index.vue',
+ *   detail_content = false,
+ *   title = '任务流程'
+ *   where path = '/dashboard/workbench/process-start';
+ * ```
+ * @param routeData
+ */
+const fixBackEndWorkbenchRoute = (routeData: Array<RemoteRoute>) => {
+  console.log('processor.ts - fixBackEndRoutes(routes: Array<RouteRecordRaw>) ... ');
+  const fix = (workbenchRoute: RemoteRoute) => {
+    workbenchRoute.componentPath = '';
+    workbenchRoute.meta.isHideAllChild = false;
+    workbenchRoute.children?.forEach((rr: RemoteRoute) => {
+      if (rr.componentName === 'WorkflowProcessApprove') {
+        rr.componentPath = 'views/pages/workflow/process-approve/Index.vue';
+        rr.meta.isDetailContent = false;
+        rr.meta.title = '审批流程';
+      }
+      if (rr.componentName === 'WorkflowProcessStart') {
+        rr.componentPath = 'views/pages/workflow/process-start/Index.vue';
+        rr.meta.isDetailContent = false;
+        rr.meta.title = '任务流程';
+      }
+    });
+    console.log('processor.ts - fixWorkbenchRoute(...) - workbenchRoute:', workbenchRoute);
+  };
+  routeData.forEach((rr: RemoteRoute) => {
+    if (rr.name === '/dashboard') {
+      rr.children?.forEach((rr1: RemoteRoute) => {
+        if (rr1.name === '/dashboard/workbench') fix(rr1);
+      });
+    }
+  });
+};
+
 export const initBackEndRoutes = async (router: Router) => {
   const response = await getRoutesFromServer();
   const routeData = response.data as Array<RemoteRoute>;
+  fixBackEndWorkbenchRoute(routeData);
   // 将后端路由数据转换为前端可识别路由格式
   const routes = convert(routeData);
   addRoutes(router, routes);
